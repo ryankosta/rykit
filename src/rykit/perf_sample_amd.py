fed773ec9affa52f59d2b537bda47d175a4f4f8d
Ryan Kosta <rkosta@ucsd.edu>
2025-12-16 12:50:31 -0800

    remove extraneous method

--- src/rykit/perf_sample_amd.py
+++ src/rykit/perf_sample_amd.py
@@ -15,15 +15,3 @@ def perf_sample_amd_uncore_event_many(
     """
     events = [f"amd_df/event={event},umask={interpret_umask(umask)}/" for event,umask in unc_events]
     return perf_sample_core_events(cmd,events)
-def perf_sample_core_event(cmd: str, core_event: str) -> int:
-    """
-    Run perf sampling for core event.
-
-    Args:
-        cmd (str): Command to run under perf.
-        core_event (str):  core event name.
-
-    Returns:
-        int: core event value 
-    """
-    return perf_sample_core_events(cmd,[core_event])[core_event]
