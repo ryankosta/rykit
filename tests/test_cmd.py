bf59806d7d9dbc6bee12ea83911e63efd44f7c30
Ryan Kosta <rkosta@ucsd.edu>
2026-04-07 11:33:10 -0700

    strip the newline

--- tests/test_cmd.py
+++ tests/test_cmd.py
@@ -1,7 +1,9 @@
 import pytest
 from rykit.cmd import run_command_read_stdout
 def test_run_command_read_stdout():
-    assert run_command_read_stdout("echo hi") == "hi"
+
+    assert run_command_read_stdout("echo hi").strip() == "hi"
+
     with pytest.raises(ValueError) as exc_info:
         run_command_read_stdout("false")
     assert str(exc_info.value) == "Command failed with exit code 1."
