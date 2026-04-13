dd267670278e15366a390e67e50d0ed1c6668566
Ryan Kosta <rkosta@ucsd.edu>
2026-04-13 16:54:19 -0700

    add numa_tools

---
+++ src/rykit/numa_tools.py
@@ -0,0 +1,10 @@
+from typing import Dict,List
+from rykit.cmd import run_command_read_stdout
+def show_numactl() -> Dict[str,List[str]]:
+    """Show results of numactl command."""
+    res = {}
+    for line in run_command_read_stdout("numactl --show").strip().split("\n"):
+        field,val = line.split(":",2)
+        val_list = val.strip().split(" ")
+        res[field] = val_list
+    return res
